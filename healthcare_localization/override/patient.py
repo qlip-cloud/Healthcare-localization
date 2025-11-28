bf2869aa2b43356f8faae4f49d979965c1a2cb30
Ashley <ashleydefort@outlook.es>
2025-11-28 20:06:33 +0000

    feat(patient_encounter): agregar formatos de impresión para la prescripción de exámenes y procedimientos
    feat(patient, patient-history): agregar las alergias registradas en el cuentro con el paciente a la historia del paciente

---
+++ healthcare_localization/override/patient.py
@@ -0,0 +1,32 @@
+import frappe
+from frappe import _
+
+@frappe.whitelist()
+def get_patient_detail(patient):
+  patient_dict = frappe.db.sql("""select * from `tabPatient` where name=%s""", (patient,), as_dict=1)
+          
+  vital_sign = frappe.db.sql("""select * from `tabVital Signs` where patient=%s order by signs_date desc limit 1""", (patient,), as_dict=1)
+
+  details = patient_dict[0]
+  if vital_sign:
+      details.update(vital_sign[0])
+
+  if not details.get("allergies"):
+      last_encounter = frappe.db.sql(
+          """
+          SELECT hco_allergies 
+          FROM `tabPatient Encounter`
+          WHERE patient = %s
+          AND docstatus = 1
+          ORDER BY encounter_date DESC
+          LIMIT 1
+          """,
+          (patient,),
+          as_dict=1
+          )
+      
+      if last_encounter and last_encounter[0].get("hco_allergies"):
+          details["allergies"] = last_encounter[0]["hco_allergies"]
+              
+  return details
+                                        
